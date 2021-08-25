f857404a2e6190e6b35e12ec7bb166354374edf0
Matteo De Micheli <matteodem@protonmail.com>
2021-08-25 12:45:49 +0200

    redo build with buddy meteor integration

--- build.sh
+++ build.sh
@@ -2,7 +2,4 @@
 npm install -g meteor-build-client
 mkdir -p dist
 cd app
-curl https://install.meteor.com/ | sh
-meteor="$HOME/.meteor"
-#cp "/opt/buildhome/.meteor/packages/meteor-tool/2.3.5/mt-os.linux.x86_64/scripts/admin/launch-meteor" /usr/bin/meteor
-meteor --help
+meteor-build-client ../dist/
