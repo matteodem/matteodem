3edc8ff587a4a7bcaffa0ceffc377add79ec8e46
Matteo De Micheli <matteodem@protonmail.com>
2021-08-25 12:38:04 +0200

    add --help command as a test for netlify

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ mkdir -p dist
 cd app
 curl https://install.meteor.com/ | sh
 #meteor="$HOME/.meteor"
-cp "/opt/buildhome/.meteor/packages/meteor-tool/2.3.5/mt-os.linux.x86_64/scripts/admin/launch-meteor" /usr/bin/meteor
-meteor-build-client ../dist/
+#cp "/opt/buildhome/.meteor/packages/meteor-tool/2.3.5/mt-os.linux.x86_64/scripts/admin/launch-meteor" /usr/bin/meteor
+/usr/bin/meteor --help
