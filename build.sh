9b0a755b247290d246f8a649c3083f8331240c1e
Matteo De Micheli <matteodem@protonmail.com>
2021-08-25 12:39:43 +0200

    try with variable renaming

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ npm install -g meteor-build-client
 mkdir -p dist
 cd app
 curl https://install.meteor.com/ | sh
-#meteor="$HOME/.meteor"
+meteor="$HOME/.meteor"
 #cp "/opt/buildhome/.meteor/packages/meteor-tool/2.3.5/mt-os.linux.x86_64/scripts/admin/launch-meteor" /usr/bin/meteor
-/usr/bin/meteor --help
+meteor --help
